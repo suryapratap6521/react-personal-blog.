269998522c84e04d55de3a56165f6c8944bf24b3
My Name <iamsunnysingh783@gmail.com>
2024-05-31 23:26:41 +0530

    Initialization of react-project

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import "./App.css";
+function App() {
+  
+
+  return (
+    <div>
+      My name is surya pratap singh
+    </div>
+  )
+}
+
+export default App
+
+echo "# react-personal-blog." >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/suryapratap6521/react-personal-blog..git
+git push -u origin main
